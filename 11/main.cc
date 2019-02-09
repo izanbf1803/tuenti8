4fe4eea846d899f453d2f149690e05f5f8a5b674
Izan Beltrán <izanbf1803@gmail.com>
2019-02-09 21:27:28 +0100

    Solved 11 using maxflow (MVC <-> MIS)

---
+++ 11/main.cc
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct FlowEdge {
+    int v, u;
+    long long cap, flow = 0;
+    FlowEdge(int v, int u, long long cap) : v(v), u(u), cap(cap) {}
+};
+
+// Source: https://cp-algorithms.com/graph/dinic.html
+struct Dinic {
+    const long long flow_inf = 1e18;
+    vector<FlowEdge> edges;
+    vector<vector<int>> adj;
+    int n, m = 0;
+    int s, t;
+    vector<int> level, ptr;
+    queue<int> q;
+
+    Dinic(int n, int s, int t) : n(n), s(s), t(t) {
+        adj.resize(n);
+        level.resize(n);
+        ptr.resize(n);
+    }
+
+    void add_edge(int v, int u, long long cap) {
+        edges.push_back(FlowEdge(v, u, cap));
+        edges.push_back(FlowEdge(u, v, 0));
+        adj[v].push_back(m);
+        adj[u].push_back(m + 1);
+        m += 2;
+    }
+
+    bool bfs() {
+        while (!q.empty()) {
+            int v = q.front();
+            q.pop();
+            for (int id : adj[v]) {
+                if (edges[id].cap - edges[id].flow < 1)
+                    continue;
+                if (level[edges[id].u] != -1)
+                    continue;
+                level[edges[id].u] = level[v] + 1;
+                q.push(edges[id].u);
+            }
+        }
+        return level[t] != -1;
+    }
+
+    long long dfs(int v, long long pushed) {
+        if (pushed == 0)
+            return 0;
+        if (v == t)
+            return pushed;
+        for (int& cid = ptr[v]; cid < (int)adj[v].size(); cid++) {
+            int id = adj[v][cid];
+            int u = edges[id].u;
+            if (level[v] + 1 != level[u] || edges[id].cap - edges[id].flow < 1)
+                continue;
+            long long tr = dfs(u, min(pushed, edges[id].cap - edges[id].flow));
+            if (tr == 0)
+                continue;
+            edges[id].flow += tr;
+            edges[id ^ 1].flow -= tr;
+            return tr;
+        }
+        return 0;
+    }
+
+    long long flow() {
+        long long f = 0;
+        while (true) {
+            fill(level.begin(), level.end(), -1);
+            level[s] = 0;
+            q.push(s);
+            if (!bfs())
+                break;
+            fill(ptr.begin(), ptr.end(), 0);
+            while (long long pushed = dfs(s, flow_inf)) {
+                f += pushed;
+            }
+        }
+        return f;
+    }
+};
+
+int main()
+{
+    const int s = 0, t = 1;
+    int c;
+    cin >> c;
+    for (int test = 0; test < c; ++test) {
+        int m, n, i;
+        cin >> m >> n >> i;
+        Dinic dinic(2+n+m, s, t);
+        for (int j = 0; j < i; ++j) {
+            int x, y;
+            cin >> x >> y;
+            dinic.add_edge(2+y, 2+n+x, 1);
+        }
+        for (int y = 0; y < n; ++y) {
+            dinic.add_edge(s, 2+y, 1);
+        }
+        for (int x = 0; x < m; ++x) {
+            dinic.add_edge(2+n+x, t, 1);
+        }
+        cout << "Case #" << test+1 << ": " << n+m-dinic.flow() << endl;
+    }
+}
